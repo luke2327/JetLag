e95bce8398a68b61ba865eca3fb4fb92b0afee92
luke2327 <qwepoi8526@gmail.com>
2023-10-27 19:26:23 +0900

    mod Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,39 +1,77 @@
+import { deleteCookie } from 'cookies-next';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+
+import { authState } from '@/store/auth';
 
 export default function Header() {
+  const setAuth = useSetRecoilState(authState);
+  const route = useRouter();
+  const auth = useRecoilValue(authState);
+  const logout = () => {
+    deleteCookie('Authorization');
+    deleteCookie('session-cookie');
+    setAuth({
+      status: 'none',
+      user: {
+        email: null,
+        age: null,
+        phone: null,
+        birthday: null,
+        sleepTime: null,
+        wakeupTime: null,
+      },
+    });
+
+    route.push('/jetlag/login');
+  };
+
   return (
     <header className='sticky top-0 flex h-12 w-[100vw] items-center'>
       <ul className='flex w-full justify-around'>
         <li>
-          <Link className='linear-ivory-text' href='/'>
+          <Link className='linear-ivory-text' href='/jetlag'>
             Jet Lag
           </Link>
         </li>
         <li>
-          <Link className='linear-ivory-text' href='/service'>
+          <Link className='linear-ivory-text' href='/jetlag/service'>
             Service
           </Link>
         </li>
         <li>
-          <Link className='linear-ivory-text' href='/'>
+          <Link className='linear-ivory-text' href='/jetlag'>
             Shopping
           </Link>
         </li>
         <li>
-          <Link className='linear-ivory-text' href='/'>
+          <Link className='linear-ivory-text' href='/jetlag'>
             Customer Center
           </Link>
         </li>
         <li>
-          <Link className='linear-ivory-text' href='/'>
+          <Link className='linear-ivory-text' href='/jetlag/myPage'>
             My Page
           </Link>
         </li>
-        <li>
-          <Link className='linear-ivory-text' href='/'>
-            Logout
-          </Link>
-        </li>
+        {auth.status === 'login' ? (
+          <li>
+            <Link
+              className='linear-ivory-text'
+              href='/jetlag/login'
+              onClick={logout}
+            >
+              Logout
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <Link className='linear-ivory-text' href='/jetlag/login'>
+              Login
+            </Link>
+          </li>
+        )}
       </ul>
     </header>
   );
