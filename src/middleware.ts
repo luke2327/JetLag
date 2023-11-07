091a279ffd5a42813567447cb43003d950ddeb21
luke2327 <qwepoi8526@gmail.com>
2023-11-07 17:49:06 +0900

    add middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,17 @@
 import createMiddleware from 'next-intl/middleware';
- 
+
+import { siteConfig } from '@/constant/config';
+
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'ko', 'ja'],
- 
+  locales: siteConfig.supportedLanguage,
+
   // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-  defaultLocale: 'ko'
+  defaultLocale: 'ko',
 });
- 
+
 export const config = {
   // Skip all paths that should not be internationalized. This example skips
   // certain folders and all pathnames with a dot (e.g. favicon.ico)
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
 };
