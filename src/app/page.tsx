3f43b6a55696833ab48938989ed32ef40d4d6426
luke2327 <qwepoi8526@gmail.com>
2023-11-08 13:49:30 +0900

    fix pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useRouter as useIntlRouter } from 'next-intl/client';
 import { useEffect } from 'react';
 
 export default function RootPage() {
   const route = useRouter();
-  const intlRoute = useIntlRouter();
 
   useEffect(() => {
-    try {
-      intlRoute.push('/');
-    } catch (e) {
-      route.push('/');
-    }
+    route.push('/');
   }, []);
 }
